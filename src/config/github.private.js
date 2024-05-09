a2ff87248c8b449083028fae78875af57e6b1a73
VeroPizzato <verizzato@gmail.com>
2024-05-09 12:38:37 -0300

    subo cambios

--- src/config/github.private.js
+++ src/config/github.private.js
@@ -2,6 +2,5 @@ module.exports = {
     appId: '886437',
     clientID: 'Iv1.837ae01fd44b8a61',
     clientSecret: '784b9c69e2df7340400973f0aafb7cdbf7f2d843',
-    callbackURL: "http://localhost:8080/api/sessions/githubcallback",
-    pepe: ''
+    callbackURL: "http://localhost:8080/api/sessions/githubcallback"   
 }
